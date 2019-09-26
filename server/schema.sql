0ad8aafdd9237ca3878e41e22ad64e80b8d15a4d
charleyarby <charleyarby@gmail.com>
2019-09-25 19:59:14 -0700

    Create Database Tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,31 @@
 CREATE DATABASE chat;
 
 USE chat;
+CREATE TABLE users (
+  userID INT,
+  userName TEXT,
+  PRIMARY KEY (userID)
+);
+
+CREATE TABLE lobbies (
+  lobbyID INT,
+  lobbyName TEXT,
+  PRIMARY KEY (lobbyID)
+);
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messageID INT,
+  userMessage TEXT,
+  lobbyID INT,
+  userID INT,
+  PRIMARY KEY (messageID),
+  FOREIGN KEY (lobbyID)
+    REFERENCES lobbies (lobbyID),
+  FOREIGN KEY (userID)
+    REFERENCES users (userID)
 );
+
 
 /* Create other tables and define schemas for them here! */
 
